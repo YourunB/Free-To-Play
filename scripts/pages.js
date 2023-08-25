e78824e110ea85c799b0bbeeeb60e4a53cdf0d9f
Yury Butskevich <124175026+YourunB@users.noreply.github.com>
2023-08-25 20:01:18 +0300

    fix: change content on main page

--- scripts/pages.js
+++ scripts/pages.js
@@ -24,9 +24,54 @@ const MainPage = {
   title: "Free To Play",
   render: (className = "container", ...rest) => {
     return `
+      <div class="header-btns__box_left">
+        <div class="sort">
+          <img class="btns" alt="Sort" title="Sort" src="assets/images/svg/sort.svg" id="btn-sort">
+          <div class="sort__param">
+            <div class="list-choice">
+              <div class="list-choice-title"><span data-language="en">Genre</span><span data-language="ru" class="unvisible">Жанр</span></div>
+                <div class="list-choice-objects">
+                  <label><input type="radio" name="genre"><span>Shooter</span></label>
+                  <label><input type="radio" name="genre"><span>MMORPG</span></label>
+                  <label><input type="radio" name="genre"><span>MOBA</span></label>
+                  <label><input type="radio" name="genre"><span>Strategy</span></label>
+                  <label><input type="radio" name="genre"><span>B.Royale</span></label>
+                  <label><input type="radio" name="genre"><span>ARPG</span></label>
+                  <label><input type="radio" name="genre"><span>Fighting</span></label>
+                  <label><input type="radio" name="genre"><span>MMO</span></label>
+                  <label><input type="radio" name="genre"><span>Racing</span></label>
+                  <label><input type="radio" name="genre"><span>Sport</span></label>
+                </div>
+            </div>
+            <div class="list-choice">
+              <div class="list-choice-title"><span data-language="en">Platform</span><span data-language="ru" class="unvisible">Платформа</span></div>
+                <div class="list-choice-objects">
+                  <label><input type="radio" name="platform"><span>PC (Windows)</span></label>
+                  <label><input type="radio" name="platform"><span>Web browser</span></label>
+                </div>
+            </div>
+            <div class="list-choice">
+              <div class="list-choice-title"><span data-language="en">Arrange</span><span data-language="ru" class="unvisible">Упорядочить</span></div>
+                <div class="list-choice-objects">
+                  <label><input type="radio" name="arrange"><span>Alphabet</span></label>
+                  <label><input type="radio" name="arrange"><span>Date</span></label>
+                </div>
+            </div>
+            <button class="button" id="btn-sort-cancel"><span data-language="en">Cancel</span><span data-language="ru" class="unvisible">Отмена</span></button>
+          </div>
+        </div>
+        <div class="search">
+          <img class="btns" alt="Search" title="Search" src="assets/images/svg/search.svg" id="btn-search">
+          <div class="search__param">
+            <input>
+          </div>
+        </div>
+      </div>
+
       <main class="container area">
         <div class="container__games ${className}">
-          <h2 class="container__games_title">Free To Play Game</h2>
+          <h2 class="container__games_title" id="games-title">Free To Play Game</h2>
+          <div class="container__games_content" id="games-box"></div>
         </div>
       </main>
     `;
