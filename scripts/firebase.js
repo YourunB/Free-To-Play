ce4adbcc0f47bdd68a9d77f8b0a8d88f032a1ed4
Yury Butskevich <124175026+YourunB@users.noreply.github.com>
2023-12-08 10:36:56 +0300

    refactor: change config db

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,11 +1,11 @@
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyAqFFJxaXXNquKy1chjyVCBffxtQ9MNhXg",
-  authDomain: "free-to-play-73b9d.firebaseapp.com",
-  databaseURL: "https://free-to-play-73b9d-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "free-to-play-73b9d",
-  storageBucket: "free-to-play-73b9d.appspot.com",
-  messagingSenderId: "678287052346",
-  appId: "1:678287052346:web:ac7a9d0ece34ceb659af03"
+  apiKey: "AIzaSyApnntzWJlDqoZm9PJp-zDYHwtQtjM4xcE",
+  authDomain: "ftp1-study.firebaseapp.com",
+  databaseURL: "https://ftp1-study-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "ftp1-study",
+  storageBucket: "ftp1-study.appspot.com",
+  messagingSenderId: "417994014326",
+  appId: "1:417994014326:web:5d6244d2173c1371eda957"
 });
 
 const myAppDB = firebaseApp.database();
